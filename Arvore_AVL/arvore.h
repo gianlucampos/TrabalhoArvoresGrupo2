b969d53068a9a0ad1c7cc1ad2155ab86ec580bff
Pachola <Pachola@Pachola-Dell-Vostro>
2017-05-27 17:51:18 -0300

    Criado as operação cria e libera Arvore Binária e libera nó.

--- Arvore_AVL/arvore.h
+++ Arvore_AVL/arvore.h
@@ -1,7 +1,11 @@
 //Aqui devemos definir as funçoes da arvore
-typedef struct No* arvore;// aqui vamos passar a estrutura do no para uma variavel arvore
+typedef struct No* ArvBin;// aqui vamos passar a estrutura do no para uma variavel arvore
 
 //Operações AVL
+ArvBin* cria_ArvBin();
+ArvBin libera_ArvBin(ArvBin *raiz); //Removerá
+void libera_NO(struct NO* no);
+
 int Busca_No();
 
 void Insere_No();
