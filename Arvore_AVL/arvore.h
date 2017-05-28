198995a42334ef51320e0ebcc1fc2e3736a10a6a
Pachola <Pachola@Pachola-Dell-Vostro>
2017-05-28 15:08:59 -0300

    Criado os métodos para saber total do no, altura e teste para saber se árvore está vazia ou não.

--- Arvore_AVL/arvore.h
+++ Arvore_AVL/arvore.h
@@ -1,10 +1,15 @@
 //Aqui devemos definir as funçoes da arvore
-typedef struct NO* ArvBin;// aqui vamos passar a estrutura do no para uma variavel arvore
+typedef struct NO* ArvBin;// Passa estrutura do no para uma variavel arvore
 
 //Operações AVL
 ArvBin* cria_ArvBin();
 ArvBin libera_ArvBin(ArvBin *raiz); //Removerá
 void libera_NO(struct NO* no);
+
+//Informações dobre a Árvore
+int estaVazia_ArvBin(ArvBin *raiz);
+int altura_ArvBin(ArvBin *raiz);
+int totalNO_ArvBin(ArvBin *raiz);
 
 int Busca_No();
 
--- Arvore_AVL/operacoes.c
+++ Arvore_AVL/operacoes.c
@@ -33,4 +33,41 @@ ArvBin libera_ArvBin(ArvBin *raiz){
     free(raiz);  //libera raiz
 }
 
-//Teste para ver o funcionamento
+int estaVazia_ArvBin(ArvBin *raiz) {
+    if(raiz == NULL){
+        return 1;
+    }
+    if(*raiz == NULL){
+        return 1;
+    }
+    return 0;
+}
+
+int altura_ArvBin(ArvBin *raiz) {
+    if(raiz == NULL){
+        return 0;
+    }
+    if (*raiz == NULL){ //caso a raiz não possua nada dentro a atura é = 0
+        return 0;
+    } 
+    int alt_esq = altura_ArvBin (& ((*raiz)->esq)); //Checar se o filho esq é diferente de nulo
+    int alt_dir = altura_ArvBin (&((*raiz)->dir)); //Checar se o filho dir é diferente de nulo
+    if(alt_esq > alt_dir) {
+        return alt_esq + 1; //caso seja ele é + 
+    } else {
+        return alt_dir + 1;
+    }
+}
+
+void int totalNO_ArvBin (ArvBin *raiz) {
+    if (raiz == NULL) {
+        return 0;
+    }
+    if (*raiz == NULL) {
+        return 0;
+    }
+    int alt_esq = totalNO_ArvBin(&((*raiz)->esq));
+    int alt_dir = totalNO_ArvBin(&((*raiz)->dir));
+    return (alt_esq + alt_dir + 1);
+}
+
